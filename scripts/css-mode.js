178af400c898414fa0982102c24f82a95126d78d
emerald-01 <teh.sjchem@gmail.com>
2023-11-03 23:16:49 +0800

    updated file

--- scripts/css-mode.js
+++ scripts/css-mode.js
@@ -1,1 +1,17 @@
 /* add your JavaScript code here! */
+
+/* Wait for DOM to be loaded before doing anything*/
+document.addEventListener("DOMContentLoaded", function() {
+  
+  /* Output a console log message of your choice one the DOM has been loaded. */
+  console.log("All systems go");
+    
+    /* Add "click" listener to button */
+    var btnMode = document.querySelector('#btn-mode');
+    btnMode.addEventListener('click', function() {
+      var element = document.body;
+      /* Click event handler should toggle the class on the body element. */
+      element.classList.toggle("darkmode");
+      });
+  });
+
