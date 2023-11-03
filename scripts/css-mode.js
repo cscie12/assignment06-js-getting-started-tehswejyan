1823285cce7c4d73c6e49c55a3b9b277d90b3cbb
emerald-01 <teh.sjchem@gmail.com>
2023-11-04 00:50:57 +0800

    3rd commit

--- scripts/css-mode.js
+++ scripts/css-mode.js
@@ -7,8 +7,8 @@ document.addEventListener("DOMContentLoaded", function() {
   console.log("All systems go");
     
     /* Add "click" listener to button */
-    var btnMode = document.querySelector('#btn-mode');
-    btnMode.addEventListener('click', function() {
+    let btnMode = document.querySelector('#btn-mode');
+    btnMode.addEventListener("click", function() {
       var element = document.body;
       /* Click event handler should toggle the class on the body element. */
       element.classList.toggle("darkmode");
